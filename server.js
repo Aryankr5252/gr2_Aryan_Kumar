b09a9605de87c7d93884323bf4a71738fbf02556
Aryankr5252 <aryanak5252@gmail.com>
2025-08-25 14:54:45 +0530

    userModel changes

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
+import { dbconnect } from "./config/db.js";
+
 const app = express();
 app.use(express.json());
 
 const port = process.env.PORT;
+
+//database is connected
+dbconnect();
 
 app.get("/", (req, res) => {
     res.send("hello");
